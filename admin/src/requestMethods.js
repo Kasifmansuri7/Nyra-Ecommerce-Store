f772251575d271b8bc6cfc738351c0ad73f9f07d
Kasifmansuri7 <kasif.mansuri.7@gmail.com>
2023-07-26 23:24:43 +0530

    updated

--- admin/src/requestMethods.js
+++ admin/src/requestMethods.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const BASE_URL = process.env.REACT_APP_API_URL;
-const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root"))?.user)
-  ?.currentUser?.accessToken;
+const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
+const currentUser = user && JSON.parse(user).currentUser;
+const TOKEN = currentUser?.accessToken;
 //  const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0YzBkM2YzM2UyNWU3YTg4OWE2ZjJmMyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY5MDM2MjU0NywiZXhwIjoxNjkwNjIxNzQ3fQ.6DbbENNRXLL9rbAOwJTc9LCgXIw5c3teD8Gsrekri-k"
 
 export const publicRequest = axios.create({
